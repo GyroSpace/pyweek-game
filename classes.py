e75007eb5a0bc74f806f8273a5dbcc9a55dc74f1
Maxix25 <maximiliano.besoain@gmail.com>
2020-09-22 15:35:34 -0300

    Scaling added to the classes file

--- classes.py
+++ classes.py
@@ -6,16 +6,15 @@ class Character(pygame.sprite.Sprite):
 		# adding all the images
 		self.images = []
 
-
-		self.images.append(pygame.image.load("Character/adventurer-run-00.png"))
-		self.images.append(pygame.image.load("Character/adventurer-run-01.png"))
-		self.images.append(pygame.image.load("Character/adventurer-run-02.png"))
-		self.images.append(pygame.image.load("Character/adventurer-run-03.png"))
-		self.images.append(pygame.image.load("Character/adventurer-run-04.png"))
-		self.images.append(pygame.image.load("Character/adventurer-run-05.png"))
+		self.images.append(pygame.transform.scale(pygame.image.load("Character/adventurer-run-00.png"), (200, 200)))
+		self.images.append(pygame.transform.scale(pygame.image.load("Character/adventurer-run-01.png"), (200, 200)))
+		self.images.append(pygame.transform.scale(pygame.image.load("Character/adventurer-run-02.png"), (200, 200)))
+		self.images.append(pygame.transform.scale(pygame.image.load("Character/adventurer-run-03.png"), (200, 200)))
+		self.images.append(pygame.transform.scale(pygame.image.load("Character/adventurer-run-04.png"), (200, 200)))
+		self.images.append(pygame.transform.scale(pygame.image.load("Character/adventurer-run-05.png"), (200, 200)))
 		self.index = 0
 		self.image = self.images[self.index]
-		self.rect = pygame.Rect(100,100,200,200)
+		self.rect = pygame.Rect(0,0,0,0)
 
 	def update(self):
 		self.index += 1
