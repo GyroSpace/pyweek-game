ea2a67e005e26f786e90cc09418622a1e528ffd5
Maxix25 <maximiliano.besoain@gmail.com>
2020-09-23 10:21:20 -0300

    Updates

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 import pygame
+import sys
 from classes import *
 from Character2 import *
 def game():
+
 	pygame.init()
 
 	screen = pygame.display.set_mode((500, 500))
@@ -16,22 +18,20 @@ def game():
 
 	isJump = False
 	jumpCount = 10
-
-	run = True
 	Char = Character()
+	The_map = Map()
+	Map_group = pygame.sprite.Group(The_map)
 	Group = pygame.sprite.Group(Char)
 	clock = pygame.time.Clock()
 
 	# Character 2
 	Char2 = Character2()
 	Group2 = pygame.sprite.Group(Char2)
-	while run:
-		pygame.time.delay(100)
-
+	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				run = False
-
+				pygame.quit()
+				sys.exit()
 		keys = pygame.key.get_pressed()
 
 		if keys[pygame.K_LEFT] and x > vel:
@@ -59,9 +59,12 @@ def game():
 				JumpCount = 10
 		Group.update()
 		Group2.update()
+		Map_group.update()
 		screen.fill((0,0,0))
 		Group.draw(screen)
 		Group2.draw(screen)
+		Map_group.draw(screen)
 		pygame.display.update()
 		clock.tick(10)
-	pygame.quit()
+pygame.quit()
+	
--- run_game.py
+++ run_game.py
@@ -1,2 +1,3 @@
 from game import *
+from classes import *
 game()
