37f820145f90542a1fb22babbbdf6731be6280c9
Maxix25 <maximiliano.besoain@gmail.com>
2020-09-23 15:15:38 -0300

    Remove some classes calls that were deleted

--- game.py
+++ game.py
@@ -19,8 +19,6 @@ def game():
 	isJump = False
 	jumpCount = 10
 	Char = Character()
-	The_map = Map()
-	Map_group = pygame.sprite.Group(The_map)
 	Group = pygame.sprite.Group(Char)
 	clock = pygame.time.Clock()
 
@@ -59,11 +57,9 @@ def game():
 				JumpCount = 10
 		Group.update()
 		Group2.update()
-		Map_group.update()
 		screen.fill((0,0,0))
 		Group.draw(screen)
 		Group2.draw(screen)
-		Map_group.draw(screen)
 		pygame.display.update()
 		clock.tick(10)
 pygame.quit()
